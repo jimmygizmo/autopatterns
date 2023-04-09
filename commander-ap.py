5a81b53947a0e0cd3b7d1c27699185625165a7bb
jimmygizmo <1915982+jimmygizmo@users.noreply.github.com>
2023-04-08 22:16:30 -0700

    argparse enhanced

--- commander-ap.py
+++ commander-ap.py
@@ -10,15 +10,17 @@ import argparse
 # AttributeError: 'Namespace' object has no attribute 'func'
 
 
-def blue(args):
-    print(f"Performing Blue command on target: {args.target}")
+def blue(function_args):
+    print(f"Performing Blue command on target: {function_args.target}")
 
 
-def green(args):
-    print(f"Performing Green command on target: {args.target}")
+def green(function_args):
+    print(f"Performing Green command on target: {function_args.target}")
 
 
 parser = argparse.ArgumentParser()
+
+
 # subparsers = parser.add_subparsers()
 
 # blue_parser = subparsers.add_parser("blue")
@@ -29,11 +31,36 @@ parser = argparse.ArgumentParser()
 # green_parser.add_argument("target")
 # green_parser.set_defaults(func=green)
 
-parser.add_argument("target")
+
+# Option -v, --verbose
+parser.add_argument("-v", "--verbose",
+                    help="Verbose output.",
+                    action="store_true",
+                    )
+
+
+# Required final argument, target of command: target
+parser.add_argument("target",
+                    help="The target of the specified command, a file path.",
+                    )
+
+
+# Conflicting options, such as a --quiet and a --verbose can be mutually-exclusive,
+# by adding a special group to add these arguments too, like this:
+# group = parser.add_mutually_exclusive_group()
+# group.add_argument("-v", "--verbose", action="store_true")
+# group.add_argument("-q", "--quiet", action="store_true")
+
+
+# Help is always included: -h, --help
+# Help can be global or if put after a command, you get command-specific help.
 
 
 if __name__ == '__main__':
     args = parser.parse_args()
+    # The following disabled line goes with the disabled subparser code (above).
     # args.func(args)
+    if args.verbose:
+        print("Output will be verbose.")
     print(args.target)
 
